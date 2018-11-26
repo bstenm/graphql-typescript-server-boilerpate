d7ff963eab48bb2765996428d7b43d3ac67589a3
Bertrand Steinmetz <bstenm@hotmail.com>
2018-11-26 15:50:40 -0500

    fixing port bug

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 GraphQL api to flatten an array of any depth
 
+test with curl:
+ curl -XPOST -H 'Content-Type:application/graphql'  -d '{ flatten(input: "[3, 4, [[5]]]") }' https://flatten-api.herokuapp.com:8080
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const port: string = process.env.PORT || (process.env.NODE === "test" ? "5000" : "4000");
+export const port: string = process.env.PORT || (process.env.NODE_ENV === "test" ? "5000" : "4000");
